04356234591a55dbaeb35f54cc77fc26baf456eb
andrsnhd <andersonh@protonmail.com>
2026-01-31 14:58:44 -0300

    Organização

--- app/config.py
+++ app/config.py
@@ -14,7 +14,11 @@ class Settings(BaseSettings):
     """
 
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+        frozen=True
     )
 
     # API
